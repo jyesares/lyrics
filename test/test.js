3cdf2e84bcdaf3d35418ae5a8247f12282d4eab6
jyesares <javiyesares@gmail.com>
2016-04-15 22:31:15 +0200

    :construction: adding babel to transpile and some settings

--- test/test.js
+++ test/test.js
@@ -1,5 +1,13 @@
-var assert = require('chai').assert;
+'use strict';
+
+import assert from 'chai';
+import http from 'http';
 
 xdescribe('Search lyric', () => {});
-describe('Get lyric', () => {});
+
+describe('Get lyric', () => {
+
+});
+
+
 xdescribe('Parse lyric', () => {});
